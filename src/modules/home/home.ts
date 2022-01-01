672e28e3f51e6ab0ba3b2c7574ea23057baf8bb4
Andrei Grini <Andrei.Grini@avanssoma.no>
2022-01-01 18:10:47 +0200

    javascript quests

---
+++ src/modules/home/home.ts
@@ -0,0 +1,15 @@
+import { button } from '../../components/button'
+import styles from './home.module.scss'
+
+export const Home = () => {
+    const title = document.createElement('h1')
+    const homeRoot = document.getElementById('homepage')
+    const aboutUsButton = button('About us', './about.html')
+
+    title.textContent = 'Hello Pro!'
+    title.classList.add('color-title')
+    homeRoot?.classList.add(styles['wrapper'])
+    //
+    homeRoot?.appendChild(title)
+    homeRoot?.appendChild(aboutUsButton)
+}
